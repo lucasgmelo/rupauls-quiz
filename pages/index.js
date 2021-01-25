103663fdfcc84753106956b5fdd23662a0526a7f
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-25 19:30:26 -0300

    init

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Page = styled.div`
+  height: 100vh;
+  background: linear-gradient(142.62deg, #CE9FFC -43.31%, #7367F0 129.93%);
+  position: relative;
+`
+
+const Bg = styled.div`
+  background-image: url(${db.bg});
+  background-size: cover;
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Page>
+      <Bg />
+      AAAAAAAA
+    </Page>
+  );
 }
