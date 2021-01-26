8f2effc1f6a0cc60d5acb4f440ea39af2707263a
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-26 02:24:56 -0300

    feat(landing): add og image

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }) {
     <Head>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
-      <meta property="OG TAG" content="/OG.png"></meta>
+      <meta property="og:image" content="/OG.png"></meta>
       <title>Quiz | Rupaul's Drag Race</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
